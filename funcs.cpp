60492deebb9d4fb6609a82590b66a79333dee2a7
Hamzah Zahid <118378960+HZahid25@users.noreply.github.com>
2024-11-23 12:48:02 +0000

    funcs.cpp

---
+++ funcs.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+
+#include "funcs.h"
+
+
+void menu_item_1() {
+  std::cout << "\n>> Menu 1\n";
+  std::cout << "\nSome code here does something useful\n";
+  // you can call a function from here that handles menu 1
+
+}
+void menu_item_2() {
+  std::cout << "\n>> Menu 2\n";
+  std::cout << "\nSome code here does something useful\n";
+  // you can call a function from here that handles menu 2
+
+}
+void menu_item_3() {
+  std::cout << "\n>> Menu 3\n";
+  std::cout << "\nSome code here does something useful\n";
+  // you can call a function from here that handles menu 3
+
+}
+void menu_item_4() {
+  std::cout << "\n>> Menu 4\n";
+  std::cout << "\nSome code here does something useful\n";
+  // you can call a function from here that handles menu 4
+
+}
