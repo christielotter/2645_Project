066a48701b7d8019269bc455e11c650d892bb27d
Aritroroy <aritroroy45@gmail.com>
2024-11-25 15:55:47 +0000

    Update funcs.h including resistor calc

--- funcs.h
+++ funcs.h
@@ -6,5 +6,9 @@ void menu_item_2();
 void menu_item_3();
 void menu_item_4();
 void go_back_to_main();
+void calculate_resistor_from_color_code();
+void combine_resistors();
+void get_npv_and_color_code_for_resistor(double resistance);
+void find_nearest_npv_resistor();
 
 #endif
